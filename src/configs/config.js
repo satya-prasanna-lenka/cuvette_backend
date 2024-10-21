01550212b6e6267b3788190fd26dd7175a448982
Satya prasanna lenka <95159702+satya-prasanna-lenka@users.noreply.github.com>
2024-10-21 20:52:27 +0530

    db atlus connection

--- src/configs/config.js
+++ src/configs/config.js
@@ -4,4 +4,4 @@ const DB_HOST_PORT = process.env.DB_HOST_PORT;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
-exports.DB_CONNECTION_URL = `mongodb://127.0.0.1:27017/cuvette`;
+exports.DB_CONNECTION_URL = `mongodb+srv://satyale39:qcMwSi86EoYfeW5K@cluster0.1axai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
